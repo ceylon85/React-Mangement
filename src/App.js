117ea44ac3be1d4ce71e71820f58137f2aefa56c
ceylon85 <canleez123@gmail.com>
2020-05-22 20:21:02 +0900

    UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React,{Component} from "react";
 import "./App.css";
 import Customer from "./components/Customer";
+import {
+  Table,
+  TableRow,
+  TableCell,
+  TableBody,
+  TableHead,
+  Paper
+} from "@material-ui/core";
+import {withStyles} from '@material-ui/core/styles';
+
+const styles= theme => ({
+  root:{
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX:'auto'
+  },
+  table:{
+    minWidth: 1080
+  }
+})
 
 const customers = [
   {
@@ -29,23 +49,41 @@ const customers = [
   },
 ];
 
-function App() {
+class App extends Component {
+  render(){
+    const {classes} = this.props;
   return (
-    <div>
-      {customers.map((c, index) => {
-        return (
-          <Customer key={index}
-            id={c.id}
-            image={c.image}
-            name={c.name}
-            birth={c.birth}
-            gende={c.gender}
-            job={c.job}
-          />
-        );
-      })}
-    </div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {customers.map((c, index) => {
+            return (
+              <Customer
+                key={index}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birth={c.birth}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
+}
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,46 +1,22 @@
 import React from "react";
-
+import { TableRow, TableCell } from "@material-ui/core";
 class Customer extends React.Component {
   render() {
     return (
-      <div>
-        <CustomerProfile
-          id={this.props.id}
-          image={this.props.image}
-          name={this.props.name}
-        />
-        <CustomerInfo
-          gender={this.props.gender}
-          birth={this.props.birth}
-          job={this.props.job}
-        />
-      </div>
+      
+        <TableRow>
+          <TableCell>{this.props.id}</TableCell>
+          <TableCell>
+            <img src={this.props.image} alt="propfile" />
+          </TableCell>
+          <TableCell>{this.props.name}</TableCell>
+          <TableCell>{this.props.birth}</TableCell>
+          <TableCell>{this.props.gender}</TableCell>
+          <TableCell>{this.props.job}</TableCell>
+        </TableRow>
+      
     );
-  }
+  } 
 }
 
-class CustomerProfile extends React.Component {
-  render() {
-    return (
-      <div>
-        <img src={this.props.image} alt="profile" />
-        <h2>
-          {this.props.name}({this.props.id})
-        </h2>
-      </div>
-    );
-  }
-}
-
-class CustomerInfo extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>{this.props.birth}</p>
-        <p>{this.props.gender}</p>
-        <p>{this.props.job}</p>
-      </div>
-    );
-  }
-}
 export default Customer;
