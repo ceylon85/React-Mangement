41d8043d1654a5c612173cbd2bf09b7be94fc179
ceylon85 <canleez123@gmail.com>
2020-05-22 19:24:36 +0900

    change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길이",
+    birth: "950921",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "박길이",
+    birth: "960511",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "최길이",
+    birth: "970320",
+    gender: "남자",
+    job: "대학생",
+  },
+];
 
 function App() {
   return (
-    <div className="gray-background">
-     <img src={logo} lat="logo" alt="logo"/>
-        
-     <h3> Let`s go
-    
-     </h3>
-    </div> 
+    <div>
+      {customers.map((c, index) => {
+        return (
+          <Customer key={index}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birth={c.birth}
+            gende={c.gender}
+            job={c.job}
+          />
+        );
+      })}
+    </div>
   );
 }
 
