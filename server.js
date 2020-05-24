ea5dd671ebe01d035b53cb868f3bec128235afce
ceylon85 <canleez123@gmail.com>
2020-05-24 20:44:51 +0900

    db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require('fs');
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -6,33 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+})
+connection.connect();
+
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: "https://placeimg.com/64/64/1",
-      name: "홍길이",
-      birth: "950921",
-      gender: "남자",
-      job: "학생",
-    },
-    {
-      id: 2,
-      image: "https://placeimg.com/64/64/2",
-      name: "박길이",
-      birth: "960511",
-      gender: "남자",
-      job: "대학생",
-    },
-    {
-      id: 3,
-      image: "https://placeimg.com/64/64/3",
-      name: "최길이",
-      birth: "970320",
-      gender: "남자",
-      job: "대학생",
-    },
-  ]);
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields)=> {
+            res.send();
+        }
+    )
+
 });
 
 app.listen(port, () => {
