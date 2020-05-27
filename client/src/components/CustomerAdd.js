9ca82469929e0d38c502512c54ab39c58fc76316
ceylon85 <canleez123@gmail.com>
2020-05-27 22:48:02 +0900

    Modal

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,8 +1,22 @@
 import React from "react";
 import Axios from "axios";
+import {
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  Button,
+} from "@material-ui/core";
 
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = (theme) => ({
+  hidden: {
+    display: "",
+  },
+});
 class CustomerAdd extends React.Component {
-    
   constructor(props) {
     super(props);
     this.state = {
@@ -12,38 +26,39 @@ class CustomerAdd extends React.Component {
       gender: "",
       job: "",
       fileName: "", //보내과 하는 파일의 이미지
+      open: false,
     };
   }
 
   handleFormSubmit = (e) => {
-      e.preventDefault();
-      this.addCustomer()
-      .then((response)=> {
-          console.log(response.data);
-          this.props.stateRefresh(); 
-      })
-      this.setState({
-          file:null,
-          userName:'',
-          birth:'',
-          gender:'',
-          job:'',
-          fileName:''
-      })
-  }
+    e.preventDefault();
+    this.addCustomer().then((response) => {
+      console.log(response.data);
+      this.props.stateRefresh();
+    });
+    this.setState({
+      file: null,
+      userName: "",
+      birth: "",
+      gender: "",
+      job: "",
+      fileName: "",
+      open: false
+    });
+  };
 
   handleFileChange = (e) => {
-      this.setState({
-          file: e.target.files[0],
-          fileName: e.target.value
-      })
-  }
+    this.setState({
+      file: e.target.files[0],
+      fileName: e.target.value,
+    });
+  };
 
   handleValueChange = (e) => {
-      let nextState= {};
-      nextState[e.target.name] = e.target.value;
-      this.setState(nextState);
-  }
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  };
 
   addCustomer = () => {
     const url = "/api/customers";
@@ -61,55 +76,86 @@ class CustomerAdd extends React.Component {
     return Axios.post(url, formData, config);
   };
 
+  handleClickOpen = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      file: null,
+      userName: "",
+      birth: "",
+      gender: "",
+      job: "",
+      fileName: "",
+      open: false,
+    });
+  };
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <form onSubmit={this.handleFormSubmit}>
-        <h1> 고객 추가 </h1>
-        프로필 이미지 :{" "}
-        <input
-          type="file"
-          name="file"
-          file={this.state.file}
-          value={this.state.fileName}
-          onChange={this.handleFileChange}
-        />
-        <br />
-        이름 :{" "}
-        <input
-          type="text"
-          name="userName"
-          value={this.state.userName}
-          onChange={this.handleValueChange}
-        />
-        <br />
-        생년월일 :{" "}
-        <input
-          type="text"
-          name="birth"
-          value={this.state.birth}
-          onChange={this.handleValueChange}
-        />{" "}
-        <br />
-        성별 :{" "}
-        <input
-          type="text"
-          name="gender"
-          value={this.state.gender}
-          onChange={this.handleValueChange}
-        />{" "}
-        <br />
-        직업 :{" "}
-        <input
-          type="text"
-          name="job"
-          value={this.state.job}
-          onChange={this.handleValueChange}
-        />{" "}
-        <br />
-        <button type="submit"> 추가하기 </button>
-      </form>
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={this.handleClickOpen}
+        >
+          고객 추가하기
+        </Button>
+        <Dialog open={this.state.open} onClose={this.state.handleClose}>
+          <DialogTitle>고객 추가</DialogTitle>
+          <DialogContent>
+              <input className={classes.hidden} accept="image/*" id="raised-button-file" type="file" file={this.state.file} value={this.state.fileName}/>
+            <label htmlFor="raised-button-file">
+                <Button variant="contained" color="primary" component="span" name="file" >
+                    {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
+                </Button>
+            </label>
+            <br/>
+            <TextField
+              label="이름"
+              type="text"
+              name="userName"
+              value={this.state.userName}
+              onChange={this.handleValueChange}
+            />
+            <br />
+            <TextField
+              label="생년월일"
+              type="text"
+              name="birth"
+              value={this.state.birth}
+              onChange={this.handleValueChange}
+            />{" "}
+            <br />
+            <TextField
+              label="성별"
+              type="text"
+              name="gender"
+              value={this.state.gender}
+              onChange={this.handleValueChange}
+            />
+            <br />
+            <TextField
+              label="직업"
+              type="text"
+              name="job"
+              value={this.state.job}
+              onChange={this.handleValueChange}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>추가</Button>
+            <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
     );
   }
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd);
